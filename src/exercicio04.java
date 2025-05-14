957a97b6cc12270a1f22c17d8cc954472b261715
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-13 22:40:36 -0300

    kxasnx

--- src/exercicio04.java
+++ src/exercicio04.java
@@ -4,17 +4,22 @@ public class exercicio04 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int valor, contador= 0, resultado;
+        int valor, contador= 0, resultado, resposta=1;
 
-        System.out.println("Informe um número: ");
-        valor = sc.nextInt();
+        while (resposta == 1) {
 
-        while (contador <= 10) {
-            resultado = valor * contador;
-            System.out.println(valor + " x " + contador + " = " + resultado);
-            contador = contador + 1;
+            System.out.println("Informe um número: ");
+            valor = sc.nextInt();
+
+            while (contador <= 10) {
+                resultado = valor * contador;
+                System.out.println(valor + " x " + contador + " = " + resultado);
+                contador = contador + 1;
+            }
+            System.out.println("Digite 1 se quiser imprimir mais um tabuada ou " +
+                    "0 para finalizar");
+            resposta = sc.nextInt();
         }
-
 
     }
 }
