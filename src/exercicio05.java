74e2a08287d6b21bb8a6df688ad1851b516da254
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-20 20:43:01 -0300

    h

--- src/exercicio05.java
+++ src/exercicio05.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class exercicio05 {
+import java.util.Scanner;
+
+public class exercicio05 {
+    public static void main(String[] args) {
+
+        //  Criar um programa em Java que imprima no vídeo os termos da série
+        //  de Fibonacci. A série eh dada por: 1 1 2 3 5 8 ...
+        //  A quantidade de termos deverá ser informada pelo usuário do
+        //  programa.
+
+        Scanner sc = new Scanner(System.in);
+        int termos, contador, x=1, y=1, z;
+
+        System.out.println("Quantos termos você quer??");
+        termos = sc.nextInt();
+
+        if ( termos == 1) {
+            System.out.println(x);
+        }
+
+        else if (termos == 2) {
+            System.out.print(x + " " + y);
+        }
+
+        else {
+            System.out.print(x + " " + y + " ");
+            contador = 3;
+
+            while ( contador <= termos) {
+                z = x + y;
+
+                System.out.print(z + " ");
+                x = y;
+                y = z;
+
+                contador = contador + 1;
+            }
+
+
+        }
+
+
+
+
+
+
+    }
 }
--- src/exercicio06.java
+++ src/exercicio06.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class exercicio06 {
+import java.util.Scanner;
+
+public class exercicio06 {
+    public static void main(String[] args) {
+
+        // Escreva um programa em Java que calcule o fatorial de um número inteiro
+        // e positivo. Por exemplo, fatorial do número 5 é representado por 5!
+        // e seu cálculo é dado da seguinte forma: 5! = 5 * 4 * 3 * 2 * 1.
+        // Por definição matemática, o fatorial de 0 é 1 e, não existe fatorial de
+        // número negativo.
+
+        Scanner sc = new Scanner(System.in);
+        int numero, contador;
+        long resultado=1;     //variavel que for guardar soma comeca com zero e variavel que vai multiplicar comeca com 1
+
+        System.out.println("Digite um número");
+        numero = sc.nextInt();
+
+        if ( numero == 0) {
+            resultado = 1;
+        }
+
+        else if ( numero < 0) {
+            System.out.println("Não existe fatorial de número negativo");
+        }
+
+        else {
+            contador = 1;
+            while ( contador <= numero) {
+                resultado = resultado * contador;
+                contador = contador + 1;
+            }
+        }
+
+        System.out.println(+ resultado);
+
+
+    }
 }
